44505091f87f6c6d9c75de4aed463637f2dd720a
Matthew Nieva <matthewcn56@gmail.com>
2021-03-22 12:33:15 -0700

    Initial Navigation Setup

---
+++ tasteBudsApp/screens/AddFriendScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "../styles.js";
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function UserProfileScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Add Friend Screen</Text>
+    </View>
+  );
+}
