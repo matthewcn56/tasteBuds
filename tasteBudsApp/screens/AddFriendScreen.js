bd8b49473a7295e9d436c6527f53858e0be10db7
Jiin <jiinkim02@gmail.com>
2021-03-22 14:05:02 -0700

    adds qr scanner to AddFriendScreen

--- tasteBudsApp/screens/AddFriendScreen.js
+++ tasteBudsApp/screens/AddFriendScreen.js
@@ -1,11 +1,66 @@
-import React from "react";
-import styles from "../styles.js";
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useContext } from 'react';
+import {AuthContext} from "../navigation/AuthProvider";
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import { db } from '../firebase/firebaseFunctions.js';
 
-export default function UserProfileScreen() {
+
+
+export default function AddFriendScreen() {
+
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const {user, setUser, logout, } = useContext(AuthContext);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    if (data === "" || data.includes(".", "#", "$", "[", "]"))
+      alert("Invalid QR Code");
+    else {
+      db.ref('users/' + data + '/displayName').once('value').then((snapshot) => {
+        if (snapshot.exists()) {
+          alert(`${snapshot.val()} has been added to your friends list!`);
+          // add to database
+        } else {
+          alert("Invalid QR Code");
+        }
+      }).catch(function(error) {
+        console.error(error);
+      });
+    }
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Add Friend Screen</Text>
+      <Text>This is our QR Reader Screen!</Text>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && (<Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />)}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
