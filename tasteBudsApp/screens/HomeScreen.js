453793d2ada56079cec6a73d02393aaf33e36a1b
Matthew Nieva <matthewcn56@gmail.com>
2021-03-22 16:39:48 -0700

    Dining Halls So Far

--- tasteBudsApp/screens/HomeScreen.js
+++ tasteBudsApp/screens/HomeScreen.js
@@ -2,16 +2,26 @@ import React, { useState, useEffect, useContext } from "react";
 import { AuthContext } from "../navigation/AuthProvider";
 import styles from "../styles.js";
 import { Text, View, Button, Image } from "react-native";
+import { db } from "../firebase/firebaseFunctions";
 
 export default function HomeScreen() {
+  //const axios = require("axios").default; //NIXED DevX API, it's not functioning
   const { user, logout } = useContext(AuthContext);
   const [userName, setUserName] = useState("");
+  const [capacities, setCapacities] = useState({});
   const [profilePic, setProfilePic] = useState(null);
-
+  //set userName on mount
   useEffect(() => {
     setUserName(user.displayName);
     setProfilePic(user.photoURL);
   }, []); //ComponentDidMount
+
+  //set the capacities on mount
+  useEffect(() => {
+    db.ref("capacities").on("value", (snapshot) => {
+      setCapacities(snapshot.val());
+    });
+  }, []);
 
   return (
     <View style={styles.container}>
