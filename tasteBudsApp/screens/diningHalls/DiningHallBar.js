a628a7048f141fb7d5a6a5c5d60345a69edcac80
Jiin <jiinkim02@gmail.com>
2021-03-22 20:48:11 -0700

    remove extra state in dining hall bar

--- tasteBudsApp/screens/diningHalls/DiningHallBar.js
+++ tasteBudsApp/screens/diningHalls/DiningHallBar.js
@@ -4,20 +4,15 @@ import { View, Text, Image } from "react-native";
 import {db} from "../../firebase/firebaseFunctions" 
 export default function DiningHallBar(props) {
 
-  const [friendImageList, setFriendImageList] = useState([]);
   const [renderFriendImages, setRenderFriendImages] = useState([]);
 
   useEffect( () => {
-    setFriendImageList([]);
     setRenderFriendImages([]);
     props.friendsInHall.map((section) => {
       console.log("current section is " + section);
       db.ref('users/' + section + '/profilePic').once('value').then((snapshot) => {
         if (snapshot.exists()) {
           console.log("current url is " + snapshot.val());
-          setFriendImageList((friendImageList) => {
-            return [...friendImageList, snapshot.val()];
-          });
           setRenderFriendImages((renderFriendImages) => {
             return [...renderFriendImages, <Image style={styles.friendImage} source={{ uri: snapshot.val() }} key={snapshot.val()}/>]
           })
