935c0838db972f890c72971b667766c655bc6e0e
Matthew Nieva <matthewcn56@gmail.com>
2021-03-24 08:48:20 -0700

    Converted Menu Scraper to Function

--- web_scraping/menuScraper.py
+++ web_scraping/menuScraper.py
@@ -1,36 +1,48 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 MEAL_TIME = "Brunch"
 REQUESTED_HALL = "BruinPlate"
-url = 'https://web.archive.org/web/20200219143919/http://menu.dining.ucla.edu/Menus/' + REQUESTED_HALL + '/Today'
-page = requests.get(url)
 
-text = page.content
+def get_menu_items_from_time_and_hall(meal_time, desired_hall):
+    url = 'https://web.archive.org/web/20200219143919/http://menu.dining.ucla.edu/Menus/' + desired_hall + '/Today'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    results = soup.find(id="main-content")
+    items = results.find_all("div", class_="menu-block")
 
-soup = BeautifulSoup(page.content, 'html.parser')
+    hallMenuItems = []
 
-results = soup.find(id="main-content")
+    for item in items:
+        currentMeal = item.find("h3", class_="col-header")
+        if currentMeal.text == MEAL_TIME:
+                menuBars=item.find_all("ul", class_ = "sect-list")
+                for menuBar in menuBars:
+                    itemSections = menuBar.find_all("li", class_ = "sect-item")
+                    for itemSection in itemSections:
+                        menuSubsections = itemSection.find_all("ul", class_="item-list")
+                        for menuSubsection in menuSubsections:
+                            menuLinks = menuSubsection.find_all("li", class_="menu-item")
+                            for menuLink in menuLinks:
+                                menuSubLinks = menuLink.find_all("span", class_="tooltip-target-wrapper")
+                                for menuSubLink in menuSubLinks:
+                                    menuItems = menuSubLink.find_all("a", class_="recipelink")
+                                    for menuItem in menuItems:
+                                        hallMenuItem = {
+                                            'itemName' : menuItem.text,
+                                            'recipeLink' : menuItem['href']
+                                        }
+                                        hallMenuItems.append(hallMenuItem)
+                                        
+    return hallMenuItems
 
-items = results.find_all("div", class_="menu-block")
+items = get_menu_items_from_time_and_hall(MEAL_TIME, REQUESTED_HALL)
+returnValue = json.dumps(items)
+#return the returnValue which is a JSON Object
 
-for item in items:
-     currentMeal = item.find("h3", class_="col-header")
-     if currentMeal.text == MEAL_TIME:
-            print("Found " + MEAL_TIME)
-            menuBars=item.find_all("ul", class_ = "sect-list")
-            for menuBar in menuBars:
-                itemSections = menuBar.find_all("li", class_ = "sect-item")
-                for itemSection in itemSections:
-                    menuSubsections = itemSection.find_all("ul", class_="item-list")
-                    for menuSubsection in menuSubsections:
-                        menuLinks = menuSubsection.find_all("li", class_="menu-item")
-                        for menuLink in menuLinks:
-                            menuSubLinks = menuLink.find_all("span", class_="tooltip-target-wrapper")
-                            for menuSubLink in menuSubLinks:
-                                menuItems = menuSubLink.find_all("a", class_="recipelink")
-                                for menuItem in menuItems:
-                                    print(menuItem.text) #SHOULD BE STORED IN A VALUE TO BE RETURNED IN API
-                                    print(menuItem['href'])
-                                    #TODO: Create JSON object with both Text & Link that gets passed in to front-e
+#For Testing Purposes
+tester = json.loads(returnValue)
+for test in tester:
+    print (test)
                 
