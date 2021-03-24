5f30627d4f951ba488d7b4e9a1ef61fc5696f85c
Matthew Nieva <matthewcn56@gmail.com>
2021-03-24 00:39:17 -0700

    Update menu.py

--- tasteBudsApp/web_scraping/menu.py
+++ tasteBudsApp/web_scraping/menu.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 url = 'https://web.archive.org/web/20200219143919/http://menu.dining.ucla.edu/Menus/Today'
 page = requests.get(url)
-
+REQUESTED_HALL = "Bruin Plate"
 text = page.content
 
 soup = BeautifulSoup(page.content, 'html.parser')
@@ -15,7 +15,7 @@ items = results.find_all("div", class_="menu-block half-col")
 for item in items:
      hall = item.find("h3", class_="col-header")
      hallName = hall.text
-     if hall.text == 'De Neve':
+     if hall.text == REQUESTED_HALL:
             menuBars=item.find_all("ul", class_ = "sect-list")
             for menuBar in menuBars:
                 itemSections = menuBar.find_all("li", class_ = "sect-item")
@@ -28,5 +28,5 @@ for item in items:
                             for menuSubLink in menuSubLinks:
                                 menuItems = menuSubLink.find_all("a", class_="recipelink")
                                 for menuItem in menuItems:
-                                    print(menuItem.text)
+                                    print(menuItem.text) #SHOULD BE STORED IN A VALUE TO BE RETURNED IN API
                 
